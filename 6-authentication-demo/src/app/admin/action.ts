4bbc119f639f1f3880fd32290cb084f92b8500e6
Md Zeeshan <imerzeeshan@gmail.com>
2025-08-12 10:23:01 +0530

    NextJS Authentication APP Using Clerk

---
+++ 6-authentication-demo/src/app/admin/action.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export async function setRole(formData: FormData) {
+  const { sessionClaims } = await auth();
+
+  if (sessionClaims?.metadata?.role !== "admin") {
+    throw new Error("Not Authorized");
+  }
+
+  const client = await clerkClient();
+  const id = formData.get("id") as string;
+  const role = formData.get("role") as string;
+
+  try {
+    await client.users.updateUser(id, {
+      publicMetadata: { role },
+    });
+    revalidatePath("/admin");
+  } catch (error) {
+    throw new Error("Failed to set role");
+  }
+}
+
+export async function removeRole(formData: FormData) {
+  const { sessionClaims } = await auth();
+
+  if (sessionClaims?.metadata?.role !== "admin") {
+    throw new Error("Not Authorized");
+  }
+
+  const client = await clerkClient();
+  const id = formData.get("id") as string;
+
+  try {
+    await client.users.updateUser(id, {
+      publicMetadata: { role: null },
+    });
+    revalidatePath("/admin");
+  } catch (error) {
+    throw new Error("Failed to remove role");
+  }
+}
