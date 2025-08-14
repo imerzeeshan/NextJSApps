428e88dacc136837a8f5b55c0bf4ce691f28e7b4
Md Zeeshan <imerzeeshan@gmail.com>
2025-08-14 15:50:43 +0530

    adding one more route image upload (o_o)

--- 6-authentication-demo/src/app/api/image/[id]/route.ts
+++ 6-authentication-demo/src/app/api/image/[id]/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(
-  req: NextRequest,
+  _req: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
   try {
