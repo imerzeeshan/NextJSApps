b1a49c281e34078e19bcc013d35a9d11e89ca65a
MD ZEESHAN <76512429+imerzeeshan@users.noreply.github.com>
2025-08-15 21:22:52 +0530

    Update navigation.tsx

--- 6-authentication-demo/src/components/navigation.tsx
+++ 6-authentication-demo/src/components/navigation.tsx
@@ -272,6 +272,12 @@ export const Navigation = () => {
             >
               Contact
             </Link>
+            <Link
+                className={`text-sm font-medium transition-colors hover:text-blue-600 dark:hover:text-blue-400`}
+                href={"/gallery"}
+              >
+                Gallery
+              </Link>
             {isAdmin && (
               <Link
                 className={`block px-3 py-2 rounded-md text-base font-medium text-gray-700 dark:text-gray-300 
