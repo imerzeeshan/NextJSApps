4c9c6f92aa085192ffe57951bd6002e99a78cecd
Md Zeeshan <imerzeeshan@gmail.com>
2025-08-15 15:40:16 +0530

    Updating functionality and optimizing (o_o)

--- 6-authentication-demo/src/app/api/image/[id]/route.ts
+++ 6-authentication-demo/src/app/api/image/[id]/route.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(
-  req: NextRequest,
+  _req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
